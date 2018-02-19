8bedf42548d948fe354339fce8cdaef1ba48b381
Shane Holloway <shane.holloway@ieee.org>
2018-02-19 14:08:01 -0700

    Refactored to use react-subscription-view. Updated rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,57 +1,53 @@
 import pkg from './package.json'
 import {minify} from 'uglify-es'
 import rpi_uglify from 'rollup-plugin-uglify'
-import rpi_babel from 'rollup-plugin-babel'
+import rpi_resolve from 'rollup-plugin-node-resolve'
+import rpi_jsy from 'rollup-plugin-jsy-babel'
 
 const sourcemap = 'inline'
-
 const external = ['react']
 
-const plugins = [jsy_plugin()]
+const plugins = [rpi_resolve({module: true, jsnext: true}), rpi_jsy()]
 const ugly = { compress: {warnings: false}, output: {comments: false}, sourceMap: false }
 const prod_plugins = plugins.concat([rpi_uglify(ugly, minify)])
 
 export default [
 	{ input: 'code/index.jsy',
 		output: [
-      { file: pkg.main, format: 'cjs', exports:'named' },
-      { file: pkg.module, format: 'es' }],
-    sourcemap, external, plugins },
-
-	{ input: 'code/index.jsy',
-		output: { file: pkg.browser, format: 'amd', exports:'named' },
-    external, plugins: prod_plugins },
-
+      { file: pkg.main, format: 'cjs', sourcemap, exports:'named' },
+      { file: pkg.module, format: 'es', sourcemap }],
+    external, plugins },
 
 	{ input: 'code/evtbus.jsy',
 		output: [
-      { file: 'dist/evtbus.js', format: 'cjs' },
-      { file: 'dist/evtbus.mjs', format: 'es' }],
-    sourcemap, external, plugins },
-
-	{ input: 'code/evtbus.jsy',
-		output: { file: 'dist/evtbus.amd.js', format: 'amd' },
-    external, plugins: prod_plugins },
-
+      { file: 'cjs/evtbus.js', format: 'cjs', sourcemap },
+      { file: 'esm/evtbus.js', format: 'es', sourcemap },
+      { file: 'umd/evtbus.js', format: 'umd', sourcemap, name: 'evtbus' },
+    ],
+    external, plugins },
 
 	{ input: 'code/react.jsy',
 		output: [
-      { file: 'dist/react.js', format: 'cjs' },
-      { file: 'dist/react.mjs', format: 'es' }],
-    sourcemap, external, plugins },
-
-	{ input: 'code/react.jsy',
-		output: { file: 'dist/react.amd.js', format: 'amd' },
-    external, plugins: prod_plugins },
-
-]
+      { file: 'cjs/react.js', format: 'cjs', sourcemap },
+      { file: 'esm/react.js', format: 'es', sourcemap },
+      { file: 'umd/react.js', format: 'umd', sourcemap, name: 'evtbus' },
+    ],
+    external, plugins },
 
 
-function jsy_plugin() {
-  const jsy_preset = [ 'jsy/lean', { no_stage_3: true, modules: false } ]
-  return rpi_babel({
-    exclude: 'node_modules/**',
-    presets: [ jsy_preset ],
-    plugins: [],
-    babelrc: false }) }
+  prod_plugins &&
+    { input: 'code/index.jsy',
+      output: { file: pkg.browser, format: 'umd', name: 'evtbus-all', exports:'named' },
+      external, plugins: prod_plugins },
 
+  prod_plugins &&
+    { input: 'code/evtbus.jsy',
+      output: { file: 'umd/evtbus.min.js', format: 'umd', name: 'evtbus' },
+      external, plugins: prod_plugins },
+
+  prod_plugins &&
+    { input: 'code/react.jsy',
+      output: { file: 'umd/react.min.js', format: 'umd', name: 'evtbus' },
+      external, plugins: prod_plugins },
+
+].filter(e=>e)
